4862bad66354efbf9cdda5d71f9e9bf71ecefc9a
Edwin Wu <edwinwu2017@u.northwestern.edu>
2016-02-01 23:44:25 -0600

    Basic Buttons
    Button functionality

--- Gamev1/Assets/Scripts/ClickTest.cs
+++ Gamev1/Assets/Scripts/ClickTest.cs
@@ -2,31 +2,13 @@
 using System.Collections;
 
 public class ClickTest: MonoBehaviour {
+    
+
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-
-//    void ClickSelect()
-//    {
-//        //Converting Mouse Pos to 2D (vector2) World Pos
-//        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//        //Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-//        //RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
-
-//        //if (hit)
-//        //{
-//        //    Debug.Log(hit.transform.name);
-//        //    return hit.transform.gameObject;
-//        //}
-//        //else return null;
-
-//        RaycastHit2D hitInfo = Physics2D.Raycast(mouseWorldPosition, Vector2.zero);
-//        if(hitInfo != null
-//        {
-//    Debug.Log( hitInfo.rigidbody.gameObject.name );
-//}
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(0)){
@@ -36,14 +18,32 @@ public class ClickTest: MonoBehaviour {
             if(hit.collider != null)
             {
                 Debug.Log("object clicked: "+hit.collider.name);
+                checkWindow0 = true;
             }
             if(hit.collider == null)
             {
                 Debug.Log("null");
+                checkWindow0 = false;
             }
         }
 	
 	}
 
+    public bool checkWindow0 = false;
 
+    void OnGUI()
+    {
+        if (checkWindow0)
+            GUI.Window(0, new Rect(110, 10, 310, 60), DoWindow0, "Basic Window");
+    }
+
+    void DoWindow0(int windowID)
+    {
+        GUI.Button(new Rect(10, 30, 150, 20), "Increase Popularity");
+        GUI.Button(new Rect(160, 30, 150, 20), "Increase Money");
+    }
+
+
+
+   
 }
