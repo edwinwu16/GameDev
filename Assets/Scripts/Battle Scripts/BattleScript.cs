c0ae41eba2ee644d48e4d7cbd991c572c51b3eb6
Sam Cohen <samc1213@gmail.com>
2016-02-09 20:49:07 -0600

    getting turns working and little text at the bottom of battles

--- Assets/Scripts/Battle Scripts/BattleScript.cs
+++ Assets/Scripts/Battle Scripts/BattleScript.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+//using System;
 
 public class BattleScript : MonoBehaviour {
 	public GameObject trump;
 	public GameObject hilary;
+//	private bool _textisavailable = true;
 	private Dictionary<GameObject, List<Attack>> movelist = new Dictionary<GameObject,  List<Attack>>();
+	private bool _myturn;
+	public Text poketext;
 	public class Attack{
 		public string name;
 		public float dmg;
@@ -17,27 +22,85 @@ public class BattleScript : MonoBehaviour {
 	}
 	// Use this for initialization
 	void Start () {
+		_myturn = false;
 		List<Attack> hilaryattacks = new List<Attack> ();
 		hilaryattacks.Add(new Attack ("rawr", 0.1f));
+		hilaryattacks.Add(new Attack ("boom!", 0.2f));
+		hilaryattacks.Add(new Attack ("heyoo", 0.18f));
+		hilaryattacks.Add(new Attack ("eat that!", 0.1f));
 		movelist.Add(hilary, hilaryattacks);
-		Debug.Log (movelist [hilary][0].name);
+
+		List<Attack> trumpattacks = new List<Attack> ();
+		trumpattacks.Add(new Attack ("booo", 0.1f));
+		trumpattacks.Add(new Attack ("go catz!", 0.2f));
+		trumpattacks.Add(new Attack ("eat me", 0.18f));
+		trumpattacks.Add(new Attack ("hiyaaa!", 0.21f));
+		movelist.Add(trump, trumpattacks);
+
+		BeginMyTurn ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.A)) {
-			
-			trump.GetComponent<FighterScript> ().health -= movelist[hilary][0].dmg;
+		if (_myturn) {
+			if (Input.GetKeyDown (KeyCode.A)) {
+				GenerateMyAttack (0, trump, hilary);
+				EndMyTurn ();
+				GenerateOpponentAttack (trump);
+			}
+			if (Input.GetKeyDown (KeyCode.S)) {
+				GenerateMyAttack (1, trump, hilary);
+				EndMyTurn ();
+				GenerateOpponentAttack (trump);
+			}
+			if (Input.GetKeyDown (KeyCode.D)) {
+				GenerateMyAttack (2, trump, hilary);
+				EndMyTurn ();
+				GenerateOpponentAttack (trump);
+			}
+			if (Input.GetKeyDown (KeyCode.F)) {
+				GenerateMyAttack (3, trump, hilary);
+				EndMyTurn ();
+				GenerateOpponentAttack (trump);
+			}
 		}
-		if (Input.GetKeyDown (KeyCode.S)) {
-			Debug.Log ("S key was pressed");
+	}
+	void BeginMyTurn() {
+		_myturn = true;
+	}
+	void EndMyTurn() {
+		_myturn = false;
+	}
+	void GenerateOpponentAttack(GameObject opponent) {
+//		Debug.Log (Random.value);
+//		yield return new WaitForSeconds(1);
+		float randf = Random.value * movelist [opponent].Count;
+		int randi = (int) randf;
+		Debug.Log (randi);
+		Attack randattack = movelist [opponent] [randi];
+		hilary.GetComponent<FighterScript> ().health -= randattack.dmg;
+		string opponentname = opponent.GetComponent<FighterScript> ().name;
+		StartCoroutine(AnimateText(opponentname + " used " + randattack.name));
+		BeginMyTurn ();
+	}
+
+	void GenerateMyAttack(int attackindex, GameObject opponent, GameObject me) {
+		Attack attack = movelist [me] [attackindex];
+		opponent.GetComponent<FighterScript> ().health -= attack.dmg;
+//		while (!_textisavailable) {
+//		}
+		StartCoroutine(AnimateText("You used " + attack.name));
+	}
+
+	IEnumerator AnimateText(string strComplete){
+//		_textisavailable = false;
+		int i = 0;
+		string str = "";
+		while( i < strComplete.Length ){
+			str += strComplete[i++];
+			poketext.GetComponent<Text> ().text = str;
+			yield return new WaitForSeconds(0.03F);
 		}
-		if (Input.GetKeyDown (KeyCode.D)) {
-			Debug.Log ("D key was pressed");
-		}
-		if (Input.GetKeyDown (KeyCode.F)) {
-			Debug.Log ("space key was F");
-		}
-		
+//		_textisavailable = true;
 	}
 }
--- Assets/Scripts/Battle Scripts/FighterScript.cs
+++ Assets/Scripts/Battle Scripts/FighterScript.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class FighterScript : MonoBehaviour {
 	public float health = 1f;
 	public GameObject fighter;
-
+	public string name;
 	// Use this for initialization
 	void Start () {
 	
